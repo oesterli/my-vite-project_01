115ed2715183e0f53f096083a08d9bbb66cf4f36
oesterli <nils.oesterling@gmx.net>
2026-06-21 10:47:43 +0200

    cesium viewer structure cleaned

--- my-vite-lit-ts-app/src/main.ts
+++ my-vite-lit-ts-app/src/main.ts
@@ -1,11 +1,2 @@
-import { render } from 'lit';
-// @ts-ignore: side-effect import for CSS handled by Vite
 import './styles.css';
 import './app/app-root';
-
-const root = document.querySelector('#app');
-if (!root) {
-    throw new Error('Could not find #app in DOM');
-}
-
-render(null, root);
