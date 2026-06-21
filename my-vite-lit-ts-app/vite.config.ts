b562af7a0e2ccec26977243e4e5fc217b39c41c7
oesterli <nils.oesterling@gmx.net>
2026-06-21 09:26:33 +0200

    vite config adopted for cesium

--- my-vite-lit-ts-app/vite.config.ts
+++ my-vite-lit-ts-app/vite.config.ts
@@ -1,11 +1,39 @@
 import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+import path from 'path';
 
 export default defineConfig({
-server: {
+  server: {
     port: 5173,
     open: true,
-},
-define: {
+  },
+
+  define: {
     CESIUM_BASE_URL: JSON.stringify('/cesium'),
   },
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'node_modules/cesium/Build/Cesium/Workers',
+          dest: 'cesium',
+        },
+        {
+          src: 'node_modules/cesium/Build/Cesium/Assets',
+          dest: 'cesium',
+        },
+        {
+          src: 'node_modules/cesium/Build/Cesium/Widgets',
+          dest: 'cesium',
+        },
+      ],
+    }),
+  ],
 });
